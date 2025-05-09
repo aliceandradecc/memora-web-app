bc5c69840cb85b957b9905ba8279a6ab9cf9edef
Alice Ferreira <alice.fandrade@a.ucb.br>
2025-05-09 03:05:13 -0300

    update: app.js, models e services; add: enums

--- backend/src/services/LogInService.js
+++ backend/src/services/LogInService.js
@@ -1,5 +1,27 @@
-const LogIn = require("../models/LogInModel")
+import { LogIn } from "../models/LogInModel.js"
 
-class LogInService {
-    
+export class LogInService {
+    constructor() {
+        this._logins = []
+        this._proximo_id = 1
+    }
+
+    // gerar novo login
+    gerarLogIn(data_login, data_expiracao, id_administrador) {
+        const novo_login = new LogIn(this._proximo_id++, data_login, data_expiracao, id_administrador)
+        this._logins.push(novo_login)
+        return novo_login
+    }
+
+    // buscar 1
+    buscarLogIn(id_login) {
+        const login = this._logins.find((item) => item.id_login === id_login)
+        if (!login) throw new Error("Log in não encontrado.")
+        return login
+    }
+
+    // listar todos
+    get logins() {
+        return this._logins
+    }
 }
--- backend/src/services/PermissaoService.js
+++ backend/src/services/PermissaoService.js
@@ -1,5 +1,42 @@
-const Permissao = require("../models/PermissaoModel")
+import { Permissao } from "../models/PermissaoModel.js"
 
-class PermissaoService {
-    
+export class PermissaoService {
+    constructor() {
+        this._permissoes = []
+        this._proximo_id = 1
+    }
+
+    // criar nova
+    criarPermissao(descricao, nome) {
+        const nova_permissao = new Permissao(this._proximo_id++, descricao, nome)
+        this._permissoes.push(nova_permissao)
+        return nova_permissao
+    }
+
+    // editar
+    editarPermissao(nome, novo_nome, nova_descricao) {
+        const permissao = this.buscarPermissao(nome)
+        permissao.nome = novo_nome
+        permissao.descricao = nova_descricao
+        return descricao
+    }
+
+    // deletar
+    deletarPermissao(id_permissao) {
+        const tamanho_original = this._permissoes.length;
+        this._permissoes = this._permissoes.filter(item => item.id_permissao !== id_permissao)
+        if (this._permissoes.length === tamanho_original) throw new Error("Permissão não encontrada.")
+    }
+
+    // buscar 1
+    buscarPermissao(nome) {
+        const permissao = this._permissoes.find((item) => item.nome === nome)
+        if (!permissao) throw new Error("Permissão não encontrada.")
+        return permissao
+    }
+
+    // listar todas
+    get permissoes() {
+        return this._permissoes
+    }
 }
