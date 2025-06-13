a7c80a1ed4a2a9e8039096d75d5ba26d539b5ffb
Alice Ferreira <alice.fandrade@a.ucb.br>
2025-06-13 08:56:27 -0300

    fix: typo

--- frontend/tela-login/src/components/Menu/Menu.tsx
+++ frontend/tela-login/src/components/Menu/Menu.tsx
@@ -4,36 +4,36 @@ import Add from "@/assets/add-square.png";
 import Search from "@/assets/search.png";
 import Input from "../Input/Input";
 
-
 type Menuprops = {
-    page: string
-}
+  page: string;
+};
 
+export default function Menu({ page }: Menuprops) {
+  let title = "Cadastrar administrador";
 
-export default function Menu({page}: Menuprops) {
+  if (page == "administrador") {
+    title = "Cadastrar administrador";
+  } else if (page == "permissao") {
+    title = "Criar nova permissão";
+  } else if (page == "publicacao") {
+    title = "Adicionar nova publicação";
+  }
 
-    let title = "Cadastrar admnistrador";
+  return (
+    <nav className={styles.container}>
+      <button className={styles.register}>
+        <Image src={Add} alt="Register" width={30} height={30} />
+        <p className={styles.label}>{title}</p>
+      </button>
 
-
-    if(page == "administrador") {
-        title = "Cadastrar admnistrador";
-    } else if(page == "permissao") {
-        title = "Criar nova permissão";
-    } else if(page == "publicacao") {
-        title = "Adicionar nova publicação";
-    }
-
-    return (
-        <nav className={styles.container}>
-            <button className={styles.register}>
-                <Image src={Add} alt="Register" width={30} height={30}/>
-                <p className={styles.label}>{title}</p>
-            </button>
-
-            <div className={styles.search}>
-                <Image src={Search} alt="Search" width={25} height={25}/>
-                <Input label="" placeholder="Buscar administrador" className={styles.inputSearch}/>
-            </div>
-        </nav>
-    );
+      <div className={styles.search}>
+        <Image src={Search} alt="Search" width={25} height={25} />
+        <Input
+          label=""
+          placeholder="Buscar administrador"
+          className={styles.inputSearch}
+        />
+      </div>
+    </nav>
+  );
 }
